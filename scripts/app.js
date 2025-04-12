3c8ffc4f3fb6ce66767993ccc0917cacdffd9a94
369Elis <emanuil.leskaj@gmail.com>
2025-04-12 14:07:12 +0100

    ✅ Fix: DB connection now working with Railway .env

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
-require("dotenv").config({ path: "../database.env" }); // Load environment variables
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const path = require("path");
 const progressRoutes = require("./progressRoutes");
 const examRoutes = require("./examRoutes");
 
